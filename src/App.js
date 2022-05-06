7759085f87b3a62d46735c14557823c03be420f5
Sukriti Thapa <thapa.sukriti@gmail.com>
2022-05-06 13:49:59 -0400

    add design and menu

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 //basic functional component
 import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import { layout, Typography, Space } from "antd";
+
+import { Navbar } from "./components";
+import "./App.css";
 
 const App = () => {
   return (
-    <div>
-      <h1>Cryptoverse</h1>
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main"></div>
+      <div className="footer"></div>
     </div>
   );
 };
